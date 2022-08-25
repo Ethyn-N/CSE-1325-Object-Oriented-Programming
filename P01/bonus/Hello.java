e83f93c2969d056556ef7756ae6218aa2dc1d6bb
Yaleruby <ethynnguyen@yahoo.com>
2022-08-24 20:40:40 -0500

    P01 bonus

---
+++ P01/bonus/Hello.java
@@ -0,0 +1,17 @@
+package cse1325.P01.bonus;
+
+import java.util.Scanner;
+
+class Hello {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        String name = input.next();
+        System.out.println("Hello, " + name + "!");
+
+        input.close();
+        
+    }
+}
