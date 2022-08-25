fc33e036c3bc9028d6fd880590f17040d513539f
Yaleruby <ethynnguyen@yahoo.com>
2022-08-25 11:55:54 -0500

    P01 extreme_bonus + P01 full_credit update

---
+++ P01/extreme_bonus/Hello.java
@@ -0,0 +1,10 @@
+package cse1325.P01.extreme_bonus;
+
+class Hello {
+    public static void main(String[] args) {
+
+        String username = System.getProperty("user.name");
+        System.out.println("Hello, " + username + "!");
+
+    }
+}
--- P01/full_credit/Hello.java
+++ P01/full_credit/Hello.java
@@ -1,3 +1,5 @@
+package cse1325.P01.full_credit;
+
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Yaleruby!"); 
