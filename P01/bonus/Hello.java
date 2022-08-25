82db85ed2cf855bb47d367dc3026efe31de7d646
Yaleruby <ethynnguyen@yahoo.com>
2022-08-24 21:09:08 -0500

    P01 bonus

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -8,9 +8,9 @@ class Hello {
         Scanner input = new Scanner(System.in);
 
         System.out.print("What is your name? ");
-        String name = input.next();
+        String name = input.nextLine();
         System.out.println("Hello, " + name + "!");
-
+        
         input.close();
         
     }
