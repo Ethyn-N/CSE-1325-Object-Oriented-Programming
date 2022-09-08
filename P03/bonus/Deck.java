56aa55c284045056f5ad7fe3afba8e1d8a3cd912
Yaleruby <ethynnguyen@yahoo.com>
2022-09-07 21:55:33 -0500

    P02 bonus

---
+++ P03/bonus/Deck.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+import java.util.Collections;
+
+class Deck {
+    public class DeckEmpty extends IndexOutOfBoundsException { 
+        public DeckEmpty(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    public Deck() {
+        deck = new Stack<Card>();
+        for (int i = Rank.MIN; i <= Rank.MAX; i++) {
+            Rank rank = new Rank(i);
+            for (Suit suit: Suit.values()) { 
+                Card card = new Card(rank, suit);
+                deck.push(card);
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(deck);
+    }
+
+    public Card deal() {
+        if (isEmpty()) 
+            throw new DeckEmpty("Deck is empty!");
+        else
+            return deck.pop();
+    }
+
+    public boolean isEmpty() {
+        if (deck.empty()) 
+            return true;
+        else
+            return false;
+    }
+
+    private Stack<Card> deck;
+}
---
+++ P03/bonus/Suit.java
@@ -0,0 +1,5 @@
+enum Suit {
+    U,
+    T,
+    A;
+}
