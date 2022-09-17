0a91c0e7e504fb0a250f7f352c5d241dc9e252a0
Yaleruby <ethynnguyen@yahoo.com>
2022-09-17 18:44:05 -0500

    P04 ElectricVehicle.java

--- P04/full_credit/ElectricVehicle.java
+++ P04/full_credit/ElectricVehicle.java
@@ -1,19 +1,21 @@
 public class ElectricVehicle extends Vehicle {
-    public ElectricVehicle(int year, String make, String model, BodyStyle bodyStyle) {
+    public ElectricVehicle(int year, String make, String model, BodyStyle bodyStyle, double whPerMile, double kwhInBattery) {
         super(year, make, model, bodyStyle);
+        this.whPerMile = whPerMile;
+        this.kwhInBattery = kwhInBattery;
     }
 
     public static double centsPerKwhOfElectricty = Double.NaN;
 
     @Override
     public double range() {
-        return kwhinBattery/(whPerMile/1000);
+        return kwhInBattery/(whPerMile/1000);
     }
 
     @Override
     public double fuelConsumed(double miles) {
         double fuelConsumed = miles*(whPerMile/1000);
-        if (fuelConsumed > kwhinBattery)
+        if (fuelConsumed > kwhInBattery)
             throw new ArithmeticException("Error: more energy consumed than available in battery");
         else 
             return fuelConsumed;
@@ -25,5 +27,5 @@ public class ElectricVehicle extends Vehicle {
     }
 
     private double whPerMile;
-    private double kwhinBattery;
+    private double kwhInBattery;
 }
