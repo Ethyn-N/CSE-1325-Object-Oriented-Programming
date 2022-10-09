454a1dc2d7fb824336a7ecd1be2b595e8ef1d07e
Yaleruby <ethynnguyen@yahoo.com>
2022-10-08 21:39:21 -0500

    MICE.java

--- P06/MICE.java
+++ P06/MICE.java
@@ -1,5 +1,8 @@
+import gui.MainWin;
+
 class MICE {
     public static void main(String[] args) {
-        
+        MainWin mainWin = new MainWin("Sundae Fundae Emporium");
+        mainWin.setVisible(true);
     }
 }
