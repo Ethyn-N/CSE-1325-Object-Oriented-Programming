5ae30b751f0ba409c21ac456c6aab4bf8f20e86c
Yaleruby <ethynnguyen@yahoo.com>
2022-10-16 00:28:09 -0500

    Added saving to Emporium

--- P07/emporium/Emporium.java
+++ P07/emporium/Emporium.java
@@ -8,7 +8,53 @@ import product.MixIn;
 import product.Scoop;
 import product.Item;
 
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Emporium {
+    public Emporium(){}
+
+    public Emporium(BufferedReader in) throws IOException {
+        int iceCreamSize = Integer.parseInt(in.readLine());
+
+        for(int i = 0; i < iceCreamSize; i++) {
+            iceCreamFlavors.add(new IceCreamFlavor(in));
+        }
+
+        int mixInSize = Integer.parseInt(in.readLine());
+
+        for(int i = 0; i < mixInSize; i++) {
+            mixInFlavors.add(new MixInFlavor(in));
+        }
+
+        int scoopSize = Integer.parseInt(in.readLine());
+
+        for(int i = 0; i < scoopSize; i++) {
+            scoops.add(new Scoop(in));
+        }
+    }
+
+    public void save(BufferedWriter out) throws IOException {
+        out.write(iceCreamFlavors.size() + "\n");
+
+        for(IceCreamFlavor iceCreamFlavor : iceCreamFlavors) {
+            iceCreamFlavor.save(out);
+        }
+
+        out.write(mixInFlavors.size() + "\n");
+
+        for(MixInFlavor mixInFlavor : mixInFlavors) {
+            mixInFlavor.save(out);
+        }
+
+        out.write(scoops.size() + "\n");
+
+        for(Scoop scoop : scoops) {
+            scoop.save(out);
+        }
+    }
+
     public void addMixInFlavor(MixInFlavor flavor) {
         mixInFlavors.add(flavor);
     }
@@ -33,7 +79,7 @@ public class Emporium {
         return scoops.toArray();
     }
 
-    private ArrayList<MixInFlavor> mixInFlavors = new ArrayList<>();
     private ArrayList<IceCreamFlavor> iceCreamFlavors = new ArrayList<>();
+    private ArrayList<MixInFlavor> mixInFlavors = new ArrayList<>();
     private ArrayList<Scoop> scoops = new ArrayList<>();
 }
