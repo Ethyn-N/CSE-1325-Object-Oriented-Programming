c08d939b53ab4b5650cc7c585440369957d86406
Yaleruby <ethynnguyen@yahoo.com>
2022-10-16 00:27:23 -0500

    Added saving to product

--- P07/product/MixIn.java
+++ P07/product/MixIn.java
@@ -1,8 +1,28 @@
 package product;
+
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class MixIn {
     public MixIn(MixInFlavor flavor, MixInAmount amount) {
         this.flavor = flavor;
         this.amount = amount;
+    }
+
+    public MixIn(BufferedReader in) throws IOException {
+        amount = MixInAmount.valueOf(in.readLine());
+
+        String name = in.readLine();
+        String description = in.readLine();
+        int cost = Integer.parseInt(in.readLine());
+        int price = Integer.parseInt(in.readLine());
+        flavor = new MixInFlavor(name, description, cost, price);
+    }
+
+    public void save(BufferedWriter out) throws IOException {
+        out.write(amount.toString() + "\n");
+        flavor.save(out);
     }
 
     @Override
--- P07/product/Scoop.java
+++ P07/product/Scoop.java
@@ -1,9 +1,37 @@
 package product;
 import java.util.ArrayList;
 
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Scoop {
     public Scoop(IceCreamFlavor flavor) {
         this.flavor = flavor;
+    }
+
+    public Scoop(BufferedReader in) throws IOException {
+        String name = in.readLine();
+        String description = in.readLine();
+        int cost = Integer.parseInt(in.readLine());
+        int price = Integer.parseInt(in.readLine());
+        flavor = new IceCreamFlavor(name, description, cost, price);
+
+        int size = Integer.parseInt(in.readLine());
+
+        for(int i = 0; i < size; i++) {
+            mixins.add(new MixIn(in));
+        }
+    }
+
+    public void save(BufferedWriter out) throws IOException {
+        flavor.save(out);
+        out.write(mixins.size() + "\n");
+
+        for(MixIn mixin : mixins) {
+            mixin.save(out);
+        }
+
     }
 
     public void addMixIn(MixIn mixIn) {
