59bf293f89fa08de7db77af40ae26de207ecc60d
Yaleruby <ethynnguyen@yahoo.com>
2022-10-29 19:22:26 -0500

    sorted.cpp

---
+++ P08/extreme_bonus/sorted.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include  <algorithm>
+
+int main(int argc, char* argv[]) {
+    std::vector<std::string> v;
+    std::string line;
+
+    while (std::getline(std::cin, line))
+        v.push_back(line);
+
+    std::sort(v.begin(), v.end());
+
+    for(auto i : v) 
+        std::cout << i << std::endl;
+
+    return 0;
+}
