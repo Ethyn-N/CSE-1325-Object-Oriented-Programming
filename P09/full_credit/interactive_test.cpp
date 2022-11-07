7a6894f0891516f80b4b2455058fb0cf1f9a7971
Yaleruby <ethynnguyen@yahoo.com>
2022-11-06 19:30:35 -0600

    interactive test

--- P09/full_credit/interactive_test.cpp
+++ P09/full_credit/interactive_test.cpp
@@ -3,11 +3,12 @@
 // This streams in two matrices and then streams out their sum
 
 #include "matrix3.h"
+#include <iomanip>
 
 int main() {
     Matrix3 m1, m2, m3;
     std::cout << "Enter two 3x3 matrices:" << std::endl;
     std::cin >> m1 >> m2;
     m3 = m1 + m2;
-    std::cout << m1 << "     +" << m2 << "      =" << m3 << std::endl;
+    std::cout << std::setw(5) << m1 << "      +\n" << m2 << "      =\n" << m3 << std::endl;
 }
